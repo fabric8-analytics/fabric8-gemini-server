af7f453c292cfa810e61a6b1a770728804c0d44f
Geetika Batra <geetika791@gmail.com>
2018-03-01 15:07:30 +0530

    Refactored ingestion to database code

--- src/utils.py
+++ src/utils.py
@@ -81,24 +81,78 @@ def validate_request_data(input_json):
 
     return True, None
 
+class DatabaseIngestion():
 
-def persist_repo_in_db(data):
-    """Store registered repository in the postgres database."""
-    req = OSIORegisteredRepos(
-            git_url=data['git_url'],
-            git_sha=data['git_sha'],
-            email_ids=data['email_ids'],
-            last_scanned_at=datetime.datetime.now()
+    @staticmethod
+    def to_object_dict(data):
+    """Convert the object of type JobToken into a dictionary."""
+        return {"OSIORegisteredRepos." + k: v for k, v in data.items()}
+
+
+    @staticmethod
+    def _update_data(session, data):
+        try:
+            entries = session.query(OSIORegisteredRepos).\
+                filter(OSIORegisteredRepos.git_url == data["git_url"]).\
+                .update(to_object_dict(data))
+            session.commit()
+        except NoResultFound:
+            return
+        except SQLAlchemyError:
+            session.rollback()
+            raise
+
+    @classmethod
+    def store_record(cls, data):
+        """Store new record and update old ones.
+        :param data: dict, describing github data
+        :return: boolean based on completion of process
+        """
+        git_url = data.get("git_url",None)
+        if git_url is None:
+            logger.info("github Url not found")
+            raise
+        session = get_session()
+        check_existing = cls.get_info(git_url)
+
+        try:
+        if check_existing["is_valid"]:
+            cls._update_data(session, data)
+            return get_info(data)
+            entry = OSIORegisteredRepos(
+                git_url=data['git_url'],
+                git_sha=data['git_sha'],
+                email_ids=data['email_ids'],
+                last_scanned_at=datetime.datetime.now()
             )
-    try:
-        _session.add(req)
-        _session.commit()
-    except SQLAlchemyError as e:
-        session.rollback()
-        message = 'persisting records in the database failed. {}.'.format(e)
-        logger.exception(message)
-        raise
-    return True
+            session.add(entry)
+            session.commit()
+        except SQLAlchemyError:
+            session.rollback()
+            raise
+        return cls.get_info(data)
+
+    @classmethod
+    def get_info(cls, search_key):
+        """Get information about github url.
+        :param search_key: github url to serach database
+        :return: record from database if exists
+        """
+        if not search_key:
+            return {'error': 'No key found', 'is_valid' : False}
+
+        session = get_session()
+
+        try:
+            entry = session.query(OSIORegisteredRepos) \
+                    .filter(OSIORegisteredRepo == search_key).one()
+        except NoResultFound:
+            logger.info("No info for search_key '%s' was found", search_key)
+            return {'error': 'No information in teh records', 'is_valid' : False}
+        except SQLAlchemyError:
+            session.rollback()
+            raise
+        return {'is_valid': True, 'data' : entry.to_dict()}
 
 
 def scan_repo(data):
@@ -127,5 +181,3 @@ class worker_selinon_flow:
         current_app.logger.debug("It took {t} seconds to start {f} flow.".format(
             t=elapsed_seconds, f=flow_name))
         return dispacher_id
-
-

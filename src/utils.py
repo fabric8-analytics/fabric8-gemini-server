40bd91970a78aa9bf2a072ff5812031f41c1dd31
Samuzzal Choudhury <samuzzal@redhat.com>
2018-03-02 13:01:48 +0530

    Initial code changes to invoke the relevant worker flow

--- src/utils.py
+++ src/utils.py
@@ -6,6 +6,7 @@ from sqlalchemy.orm.exc import NoResultFound
 from requests.adapters import HTTPAdapter
 from requests.packages.urllib3.util.retry import Retry
 from f8a_worker.models import OSIORegisteredRepos
+from f8a_worker.setup_celery import init_celery
 from selinon import run_flow
 import datetime
 import requests
@@ -86,15 +87,15 @@ def validate_request_data(input_json):
     :return: boolean, result
     """
     validate_string = "{} cannot be empty"
-    if 'git_url' not in input_json:
+    if 'git-url' not in input_json:
         validate_string = validate_string.format("git_url")
         return False, validate_string
 
-    if 'git_sha' not in input_json:
+    if 'git-sha' not in input_json:
         validate_string = validate_string.format("git_sha")
         return False, validate_string
 
-    if 'email_ids' not in input_json:
+    if 'email-ids' not in input_json:
         validate_string = validate_string.format("email_ids")
         return False, validate_string
 
@@ -190,35 +191,31 @@ class DatabaseIngestion():
         return {'is_valid': True, 'data': entry.to_dict()}
 
 
+def server_run_flow(self, flow_name, flow_args):
+    """Run a flow.
+
+    :param flow_name: name of flow to be run as stated in YAML config file
+    :param flow_args: arguments for the flow
+    :return: dispatcher ID handling flow
+    """
+    logger.debug('Running flow {}'.format(flow_name))
+    start = datetime.datetime.now()
+
+    init_celery(result_backend=False)
+    dispacher_id = run_flow(flow_name, flow_args)
+
+    elapsed_seconds = (datetime.datetime.now() - start).total_seconds()
+    logger.debug("It took {t} seconds to start {f} flow.".format(
+        t=elapsed_seconds, f=flow_name))
+    return dispacher_id
+
+
 def scan_repo(data):
     """Scan function."""
+    args = {'github_url': data['git-url'],
+            'github_sha': data['git-sha'],
+            'email_ids': data['email-ids']}
+    logger.info('Initiating scan of repo {}:{}'.format(data['git-url'], data['git-sha']))
+    server_run_flow('osioAnalysisFlow', args)
+
     return True
-
-
-class worker_selinon_flow:
-    """Worker class to initialize async flows."""
-
-    def __init__(self):
-        """Initialize class.
-
-        Intializes selinon for async functionality.
-        """
-        init_selinon()
-
-    def server_run_flow(self, flow_name, flow_args):
-        """Run a flow.
-
-        :param flow_name: name of flow to be run as stated in YAML config file
-        :param flow_args: arguments for the flow
-        :return: dispatcher ID handling flow
-        """
-        current_app.logger.debug('Running flow {}'.format(flow_name))
-        start = datetime.datetime.now()
-
-        init_celery(result_backend=False)
-        dispacher_id = run_flow(flow_name, flow_args)
-
-        elapsed_seconds = (datetime.datetime.now() - start).total_seconds()
-        current_app.logger.debug("It took {t} seconds to start {f} flow.".format(
-            t=elapsed_seconds, f=flow_name))
-        return dispacher_id

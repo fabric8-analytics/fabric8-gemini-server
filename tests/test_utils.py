99954ffef99714a649fbd227a9fc65dd6c2cdba8
Pavel Tisnovsky <ptisnovs@redhat.com>
2018-03-02 15:00:36 +0100

    Unit test import failure

---
+++ tests/test_utils.py
@@ -0,0 +1,9 @@
+"""Test module for classes and functions found in the utils module."""
+import requests
+import os
+
+import src.utils
+
+def test_validate_request_data():
+    """Test the function validate_request_data."""
+    pass

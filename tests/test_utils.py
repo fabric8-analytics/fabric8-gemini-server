a1eea0c9658ae27a9be5635ba44ea17ebb9db58e
Pavel Tisnovsky <ptisnovs@redhat.com>
2018-04-09 09:35:19 +0200

    Stubs for test_utils

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -2,9 +2,30 @@
 import requests
 import os
 
-import src.utils
+from src.utils import *
 
 
 def test_validate_request_data():
     """Test the function validate_request_data."""
-    pass
+    valid, result = validate_request_data({})
+    assert not valid
+    assert result == "git-url cannot be empty"
+
+    valid, result = validate_request_data({"git-url": "something"})
+    assert not valid
+    assert result == "git-sha cannot be empty"
+
+    valid, result = validate_request_data({"git-url": "something",
+                                           "git-sha": "something"})
+    assert not valid
+    assert result == "email-ids cannot be empty"
+
+    valid, result = validate_request_data({"git-url": "something",
+                                           "git-sha": "something",
+                                           "email-ids": "something"})
+    assert valid
+    assert not result
+
+
+if __name__ == '__main__':
+    test_validate_request_data()

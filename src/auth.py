5c9fcdc1a983d958a01465842b4c0f28c7545fda
abs51295 <aagams68@gmail.com>
2018-04-27 17:00:08 +0530

    Add authentication mechanism
    - Authentication for /register and /report endpoint is added.
    - Returns 404 if unauthorized.

---
+++ src/auth.py
@@ -0,0 +1,70 @@
+"""Authorization token handling."""
+from functools import wraps
+
+from flask import current_app, request
+import jwt
+from os import getenv
+
+
+from exceptions import HTTPError
+from utils import fetch_public_key
+
+
+def decode_token():
+    """Decode the authorization token read from the request header."""
+    token = request.headers.get('Authorization')
+    if token is None:
+        return {}
+
+    if token.startswith('Bearer '):
+        _, token = token.split(' ', 1)
+
+    pub_key = fetch_public_key(current_app)
+    audiences = getenv('BAYESIAN_JWT_AUDIENCE').split(',')
+
+    for aud in audiences:
+        try:
+            decoded_token = jwt.decode(token, pub_key, algorithms=['RS256'], audience=aud)
+        except jwt.InvalidTokenError:
+            current_app.logger.error('Auth Token could not be decoded for audience {}'.format(aud))
+            decoded_token = None
+
+        if decoded_token is not None:
+            break
+
+    if decoded_token is None:
+        raise jwt.InvalidTokenError('Auth token audience cannot be verified.')
+
+    return decoded_token
+
+
+def login_required(view):
+    """Check if the login is required and if the user can be authorized."""
+    # NOTE: the actual authentication 401 failures are commented out for now and will be
+    # uncommented as soon as we know everything works fine; right now this is purely for
+    # being able to tail logs and see if stuff is going fine
+    @wraps(view)
+    def wrapper(*args, **kwargs):
+        # Disable authentication for local setup
+        if getenv('DISABLE_AUTHENTICATION') in ('1', 'True', 'true'):
+            return view(*args, **kwargs)
+
+        lgr = current_app.logger
+
+        try:
+            decoded = decode_token()
+            if not decoded:
+                lgr.exception('Provide an Authorization token with the API request')
+                raise HTTPError(401, 'Authentication failed - token missing')
+
+            lgr.info('Successfuly authenticated user {e} using JWT'.
+                     format(e=decoded.get('email')))
+        except jwt.ExpiredSignatureError as exc:
+            lgr.exception('Expired JWT token')
+            raise HTTPError(401, 'Authentication failed - token has expired') from exc
+        except Exception as exc:
+            lgr.exception('Failed decoding JWT token')
+            raise HTTPError(401, 'Authentication failed - could not decode JWT token') from exc
+
+        return view(*args, **kwargs)
+    return wrapper
--- src/utils.py
+++ src/utils.py
@@ -238,3 +238,30 @@ def scan_repo(data):
     d_id = server_run_flow('osioAnalysisFlow', args)
     logger.info("DISPATCHER ID = {}".format(d_id))
     return True
+
+
+def fetch_public_key(app):
+    """Get public key and caches it on the app object for future use."""
+    # TODO: even though saving the key on the app object is not very nice,
+    #  it's actually safe - the worst thing that can happen is that we will
+    #  fetch and save the same value on the app object multiple times
+    if not getattr(app, 'public_key', ''):
+        keycloak_url = os.getenv('BAYESIAN_FETCH_PUBLIC_KEY', '')
+        if keycloak_url:
+            pub_key_url = keycloak_url.strip('/') + '/auth/realms/fabric8/'
+            try:
+                result = requests.get(pub_key_url, timeout=0.5)
+                app.logger.info('Fetching public key from %s, status %d, result: %s',
+                                pub_key_url, result.status_code, result.text)
+            except requests.exceptions.Timeout:
+                app.logger.error('Timeout fetching public key from %s', pub_key_url)
+                return ''
+            if result.status_code != 200:
+                return ''
+            pkey = result.json().get('public_key', '')
+            app.public_key = \
+                '-----BEGIN PUBLIC KEY-----\n{pkey}\n-----END PUBLIC KEY-----'.format(pkey=pkey)
+        else:
+            app.public_key = None
+
+    return app.public_key

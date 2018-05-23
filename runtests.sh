5a412afc53cb2363d9128a54413d0047181ecec4
Pavel Tisnovsky <ptisnovs@redhat.com>
2018-05-23 19:07:33 +0200

    More messages on CI

--- runtests.sh
+++ runtests.sh
@@ -9,6 +9,8 @@ RED=$(tput bold && tput setaf 1)
 GREEN=$(tput bold && tput setaf 2)
 YELLOW=$(tput bold && tput setaf 3)
 
+printf "${NORMAL}Shutting down docker-compose ..."
+
 gc() {
   retval=$?
   docker-compose -f docker-compose.yml down -v || :
@@ -28,7 +30,8 @@ function start_gemini_service {
 start_gemini_service
 
 export PYTHONPATH=`pwd`/src
-echo "Create Virtualenv for Python deps ..."
+printf "${NORMAL}Create Virtualenv for Python deps ..."
+
 function prepare_venv() {
     VIRTUALENV=`which virtualenv`
     if [ $? -eq 1 ]

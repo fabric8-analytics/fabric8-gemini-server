fcc4cd59227f12d713ec9de14ef74d614b8cb9d2
abs51295 <aagams68@gmail.com>
2018-09-19 17:06:51 +0530

    Add test for new scan endpoint
    - Tests for the newly introduced scan endpoint are added.
    - The old scan endpoint is marked as experimental endpoint.

---
+++ tests/test_user_notification.py
@@ -0,0 +1,21 @@
+"""Tests for UserNotification."""
+
+from src.notification.user_notification import UserNotification
+
+
+def test_generate_notification():
+    """Test generate notification."""
+    report = {
+        "repo_url": "http://test",
+        "vulnerable_deps": [
+            {
+                "cve_count": 0
+            },
+            {
+                "cve_count": 1
+            }
+        ],
+        "cve_count": 0
+    }
+    resp = UserNotification.generate_notification(report)
+    assert resp["data"]["attributes"]["custom"]["cve_count"] == 1

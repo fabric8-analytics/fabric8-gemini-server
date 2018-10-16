b8fa8935c90e023bc81beb83e44def04d8bc38ed
Pavel Tisnovsky <ptisnovs@redhat.com>
2018-10-16 10:51:08 +0200

    User notification test

--- tests/test_user_notification.py
+++ tests/test_user_notification.py
@@ -19,3 +19,39 @@ def test_generate_notification():
     }
     resp = UserNotification.generate_notification(report)
     assert resp["data"]["attributes"]["custom"]["cve_count"] == 1
+
+
+def test_generate_notification_2():
+    """Test generate notification."""
+    report = {
+        "repo_url": "http://test",
+        "vulnerable_deps": [
+            {
+                "cve_count": 1
+            },
+            {
+                "cve_count": 0
+            }
+        ],
+        "cve_count": 0
+    }
+    resp = UserNotification.generate_notification(report)
+    assert resp["data"]["attributes"]["custom"]["cve_count"] == 1
+
+
+def test_generate_notification_3():
+    """Test generate notification."""
+    report = {
+        "repo_url": "http://test",
+        "vulnerable_deps": [
+            {
+                "cve_count": 1
+            },
+            {
+                "cve_count": 1
+            }
+        ],
+        "cve_count": 0
+    }
+    resp = UserNotification.generate_notification(report)
+    assert resp["data"]["attributes"]["custom"]["cve_count"] == 2

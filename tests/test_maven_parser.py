f40d23e2e5bce92ad832b70a6b370f7261783124
Pavel Tisnovsky <ptisnovs@redhat.com>
2019-03-19 10:11:56 +0100

    Two new unit tests for Maven parser

--- tests/test_maven_parser.py
+++ tests/test_maven_parser.py
@@ -3,11 +3,30 @@
 from src.parsers.maven_parser import MavenParser
 from pathlib import Path
 from werkzeug.datastructures import FileStorage
+import pytest
 
 
-def test_maven_parser_output_file():
+def test_maven_parser_output_files_direct_dependencies():
     """Test maven parser."""
     with (Path(__file__).parent / "files/direct-dependencies.txt").open('rb') as f:
         resp = MavenParser.parse_output_files([FileStorage(f, filename='direct-dependencies.txt')])
         assert resp == ({"maven:resolved::",
                         "maven:org.apache.geronimo.modules:geronimo-tomcat6:2.2.1"}, set())
+
+
+def test_maven_parser_output_files_transitive_dependencies():
+    """Test maven parser."""
+    with (Path(__file__).parent / "files/transitive-dependencies.txt").open('rb') as f:
+        filename = 'transitive-dependencies.txt'
+        resp = MavenParser.parse_output_files([FileStorage(f, filename=filename)])
+        assert resp == ({"maven:resolved::",
+                        "maven:org.apache.geronimo.modules:geronimo-tomcat6:2.2.1"}, set())
+
+
+def test_maven_parser_output_files_bad_filename():
+    """Test maven parser."""
+    with (Path(__file__).parent / "files/bad-filename.txt").open('rb') as f:
+        filename = 'bad-filename.txt'
+        with pytest.raises(BadRequest) as e:
+            MavenParser.parse_output_files([FileStorage(f, filename=filename)])
+            assert e is not None

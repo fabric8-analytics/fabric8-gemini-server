76b836f1709f24bd4a239c63dc891e2b3186d5fd
jparsai <jparsai@redhat.com>
2020-11-30 18:05:47 +0530

    fix: Unify python dependency installation and update to vulnerability free versions (#222)
    * Dependency cleanup and Postgres conn modifications
    
    * Removed changes done for transaction error
    
    * Including requirements.in into tests/requirements.in
    
    Co-authored-by: Arunprasad Rajkumar <ar.arunprasad@gmail.com>
    
    * Including main requirements.in into tests/requirements.in
    
    Co-authored-by: Arunprasad Rajkumar <ar.arunprasad@gmail.com>

--- setup.py
+++ setup.py
@@ -6,29 +6,27 @@ from setuptools import setup, find_packages
 
 
 def get_requirements():
-    """Parse all packages mentioned in the 'requirements.txt' file."""
-    with open('requirements.txt') as fd:
-        lines = fd.read().splitlines()
-        reqs, dep_links = [], []
-        for line in lines:
-            if line.startswith('git+'):
-                dep_links.append(line)
-            else:
-                reqs.append(line)
-        return reqs, dep_links
+    """
+    Parse dependencies from 'requirements.in' file.
+
+    Collecting dependencies from 'requirements.in' as a list,
+    this list will be used by 'install_requires' to specify minimal dependencies
+    needed to run the application.
+    """
+    with open('requirements.in') as fd:
+        return fd.read().splitlines()
 
 
 # pip doesn't install from dependency links by default, so one should install dependencies by
 #  `pip install -r requirements.txt`, not by `pip install .`
 #  See https://github.com/pypa/pip/issues/2023
-reqs, dep_links = get_requirements()
+install_requires = get_requirements()
 
 setup(
     name='gemini-server',
     version='0.1',
     packages=find_packages(exclude=['tests', 'tests.*']),
-    install_requires=reqs,
-    dependency_links=dep_links,
+    install_requires=install_requires,
     include_package_data=True,
     author='Samuzzal Choudhury',
     author_email='samuzzal@redhat.com',
